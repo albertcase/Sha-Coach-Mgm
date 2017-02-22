73d8ae7514dddcf46582190859a5a2be5661c9e8
Demon Zhang <demon.zhang@samesamechina.com>
2017-02-22 17:35:35 +0800

    no message

---
+++ bundle/CampaignBundle/PageController.php
@@ -0,0 +1,46 @@
+<?php
+namespace CampaignBundle;
+
+use Core\Controller;
+
+class PageController extends Controller {
+
+	public function indexAction() {
+		$RedisAPI = new \Lib\RedisAPI();
+		$config = $RedisAPI->jssdkConfig($this->request->getUrl(TRUE));
+		$this->render('index', array('config' => $config));
+	}
+
+	public function testAction() {
+		$RedisAPI = new \Lib\RedisAPI();
+		$RedisAPI ->setParent(2,1);
+		$RedisAPI ->setParent(3,2);
+		$RedisAPI ->setParent(4,3);
+		$RedisAPI ->setParent(5,4);
+		$RedisAPI ->setParent(6,5);
+		$RedisAPI ->setParent(7,6);
+		$rs= $RedisAPI ->getAllParent(5);
+		var_dump($rs);
+		echo 1;exit;
+	}
+
+	public function replyAction() {
+		$request = $this->request;
+    	$fields = array(
+			'openid' => array('notnull', '120'),
+			'nickname' => array('notnull', '121'),
+			'scene_str' => array('notnull', '122'),
+		);
+		$request->validation($fields);
+		$openid = $request->request->get('openid');
+		$nickname = $request->request->get('nickname');
+		$scene_str = $request->request->get('scene_str');
+		
+		echo 1;exit;
+	}
+
+	public function clearCookieAction() {
+		setcookie('_user', json_encode($user), time(), '/');
+		$this->statusPrint('success');
+	}
+}
