7fe59f15023055b875f76abbd60a6c7e3158d1bb
Demon Zhang <demon.zhang@samesamechina.com>
2017-02-28 17:33:04 +0800

    no message

--- bundle/CampaignBundle/PageController.php
+++ bundle/CampaignBundle/PageController.php
@@ -26,7 +26,7 @@ class PageController extends Controller {
 	}
 
 	public function replyAction() {
-		$data = isset($GLOBALS['HTTP_RAW_POST_DATA']) ? $GLOBALS['HTTP_RAW_POST_DATA'] : '';
+		$data = isset($GLOBALS['HTTP_RAW_POST_DATA']) ? $GLOBALS['HTTP_RAW_POST_DATA'] : '{"openid":"oqQW1w6XC7Cd9ApryXfNsOkffrSw","nickname":"\u6211\u662f\u827e\u6d3e\u5fb7\ud83d\ude03","headimgurl":"http:\/\/wx.qlogo.cn\/mmopen\/j2lJiakBYwa3J0ib1sTBPoUH2YJFQTcI68ibica2U2ACB6JKRPicIdlHjTEBXPatIPqK0Qw9icqE7foolTSWLJFnDFbV74tiaV58BbK\/0","scene_str":"oqQW1w2hTaqyg5URFoBjSHgY47WU"}';
 		if (!$data) {
 			$data = array('status' => 'failed');
 		    $this->dataPrint($data);
@@ -38,7 +38,7 @@ class PageController extends Controller {
 
 		if($DatabaseAPI->insertReply($data)) {
 			$response = array('openid' => $data->openid, 'text' => '关注成功', 'link' => 'http://baidu.com');
-			$data = array('status' => 'success');
+			$data = array('status' => 'success', 'data' => $response);
 			$this->dataPrint($data);
 		} else {
 			$data = array('status' => 'success');
