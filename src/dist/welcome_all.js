f486f06d9d8b4c1db8f32036b3916b2520f65d97
evelyn_zhang <1054982653@qq.com>
2017-03-06 18:37:32 +0800

    hide wx option button

--- src/dist/welcome_all.js
+++ src/dist/welcome_all.js
@@ -277,6 +277,13 @@ $(document).ready(function(){
 	$('body').on('touchstart','.btn-alert-ok',function(){
 		$(this).parent().parent('.alertpop').remove();
 	});
+	wx.ready(function(){
+		//wx.hideOptionMenu({
+		//    menuList: ["menuItem:share:appMessage","menuItem:share:timeline","menuItem:share:qq","menuItem:share:weiboApp","menuItem:share:facebook","menuItem:share:QZone","menuItem:copyUrl","menuItem:openWithQQBrowser","menuItem:openWithSafari","menuItem:share:email"] // 要显示的菜单项，所有menu项见附录3
+		//});
+		wx.hideAllNonBaseMenuItem();
+
+	})
 });
 
 
