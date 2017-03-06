eb936d5b0c180b7c14ec72b2d22fb9911182cab7
Demon Zhang <demon.zhang@samesamechina.com>
2017-03-06 11:37:24 +0800

    no message

--- Lib/RedisAPI.php
+++ Lib/RedisAPI.php
@@ -109,7 +109,7 @@ class RedisAPI {
 
 	public function getKey() {
 		var_dump($this->_redis->keys('parent:*'));
-		var_dump($this->_redis->keys('sendList'));
+		var_dump($this->_redis->get('sendList'));
 		exit;
 	}
 
