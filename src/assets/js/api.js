ff1289370b1414c2fd086630eadeee93efb70a9b
evelyn_zhang <1054982653@qq.com>
2017-03-07 12:08:03 +0800

    add api

--- src/assets/js/api.js
+++ src/assets/js/api.js
@@ -24,6 +24,53 @@ Api = {
 
     },
 
+    //get prize list
+    prizeList:function(callback){
+        Common.msgBox('loading...');
+        $.ajax({
+            url:'/api/prizelist',
+            type:'POST',
+            dataType:'json',
+            success:function(data){
+                $('.ajaxpop').remove();
+                return callback(data);
+                //status=1 有库存
+            }
+        });
+
+        //return callback({
+        //    status:1,
+        //    avatar:'/src/images/qr-1.png',
+        //    score:'100'
+        //})
+
+
+    },
+
+    //check if available
+    isCheck:function(obj,callback){
+        Common.msgBox('loading...');
+        $.ajax({
+            url:'/api/check',
+            type:'POST',
+            dataType:'json',
+            data:obj,
+            success:function(data){
+                $('.ajaxpop').remove();
+                return callback(data);
+                //status=1 有库存
+            }
+        });
+
+        //return callback({
+        //    status:1,
+        //    avatar:'/src/images/qr-1.png',
+        //    score:'100'
+        //})
+
+
+    },
+
     //提交用户表单信息
     submitUserForm:function(obj,callback){
         Common.msgBox('loading...');
