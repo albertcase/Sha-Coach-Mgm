21120ba00e9809208f4553df0b8753e47598d2d2
Demon Zhang <demon.zhang@samesamechina.com>
2017-03-24 11:50:10 +0800

    no message

--- script/push.php
+++ script/push.php
@@ -1,10 +1,11 @@
 <?php
+set_time_limit(0);
 require_once "../Core/bootstrap.php";
 include_once "../config/config.php";
 include_once "../config/router.php";
 
 $RedisAPI = new \Lib\RedisAPI();
-for ($i=0; $i<200; $i++) {
+for ($i=0; $i<1000; $i++) {
 	print $RedisAPI->runScript();
 }
 
